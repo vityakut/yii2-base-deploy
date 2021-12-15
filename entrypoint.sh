45f97dd2d7429efc5f46206c7ebd461801ef56c9
vityakut <vityakut@gmail.com>
2021-12-15 13:02:48 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,7 +24,7 @@ rsync --progress -avzh \
 	--exclude='readme.md' \
 	--exclude='README.md' \
 	-e "ssh -i /root/.ssh/id_rsa" \
-	--rsync-path="sudo rsync" . $1@$2:$3
+	--rsync-path="rsync" . $1@$2:$3
 
 if [ $? -eq 0 ]
 then
